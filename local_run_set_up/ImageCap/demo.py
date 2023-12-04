9d413c2a98dc6f61401b9c30f22da2705f4baeca
Sagarkeshave <96720769+Sagarkeshave@users.noreply.github.com>
2023-12-04 15:23:51 +0530

    local setup for API

---
+++ local_run_set_up/ImageCap/demo.py
@@ -0,0 +1,9 @@
+import os
+from sys import argv
+
+# print("Number of arguments:", len(argv), "arguments")
+
+# print("Argument List:", str(argv))
+# print(argv[0])
+print((os.path.join(os.getcwd()),'ImageCap/embedB.npy'))
+
